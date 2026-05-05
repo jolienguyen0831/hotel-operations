be1fd2f870884415bc234d42738668cf98efd1a5
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-05-05 10:20:51 -0500

    create Room class

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+
+
+    public Room(double price, boolean occupied, boolean dirty, int numberOfBeds) {
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable(){
+        return !isOccupied() && !isDirty();
+    }
 }
