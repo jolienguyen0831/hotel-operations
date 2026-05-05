a549f251d28d1ffaaa1c5a901edc287ad2ef2abe
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-05-05 10:41:58 -0500

    create Employee class

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,78 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public double getRegularHours() {
+        if (getHoursWorked() <= 40) {
+            return getHoursWorked();
+        } else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeHours(){
+        if (hoursWorked > 40) {
+            return getHoursWorked() - 40;
+        }else {
+            return 0;
+        }
+    }
+
+    public double getTotalPay(){
+        double regularPay = getRegularHours() * getPayRate();
+        double overtimePay = getOvertimeHours() * 1.5 * getPayRate();
+        return regularPay + overtimePay;
+    }
+}
